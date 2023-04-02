1e94dbeb7a42fb6efce3b9c0fcb50e878c7148b9
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-02 04:19:54 +0100

    Added vim-surround

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,20 @@ vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>Telescope oldfiles<cr>", { noremap =
 
 -- Toggle auto-save
 vim.api.nvim_set_keymap("n", "<leader>n", ":ASToggle<CR>", {})
+
+-- Vim surround 
+vim.g["surround_no_mappings"] = 1
+vim.keymap.set("n", "ds", "<Plug>Dsurround")
+vim.keymap.set("n", "cs", "<Plug>Csurround")
+vim.keymap.set("n", "cS", "<Plug>CSurround")
+vim.keymap.set("n", "ys", "<Plug>Ysurround")
+vim.keymap.set("n", "yS", "<Plug>YSurround")
+vim.keymap.set("n", "yss", "<Plug>Yssurround")
+vim.keymap.set("n", "ySs", "<Plug>YSsurround")
+vim.keymap.set("n", "ySS", "<Plug>YSsurround")
+
+-- The conflicting ones. Note that `<Plug>(leap-cross-window)`
+-- _does_ work in Visual mode, if jumping to the same buffer,
+-- so in theory, `gs` could be useful for Leap too...
+vim.keymap.set("x", "gs", "<Plug>VSurround")
+vim.keymap.set("x", "gS", "<Plug>VgSurround")
