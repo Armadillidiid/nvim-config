9773433fbade05472cf0a34f402cb38a402a0ca2
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-02 20:28:21 +0100

    Added Tabnine

---
+++ lua/plugins/tabnine.lua
@@ -0,0 +1,10 @@
+return {
+  "codota/tabnine-nvim",
+  build = "./dl_binaries.sh",
+  disable_auto_comment = true,
+  accept_keymap = "<Tab>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = { gui = "#808080", cterm = 244 },
+  exclude_filetypes = { "TelescopePrompt" },
+}
