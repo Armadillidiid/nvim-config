6e284b6f783c416398e7a7211f53b75429aacf9d
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-02 18:23:45 +0100

    Telescope old files, find file and mapping alias

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,28 +2,44 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Map "jk" to exit Insert mode
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
+local Util = require("lazyvim.util")
 
--- Map "Ctrl+p" to telescope oldfiles
-vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>Telescope oldfiles<cr>", { noremap = true })
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- Map "jk" to exit Insert mode
+map("i", "jk", "<Esc>", { noremap = true })
+
+-- Map "Ctrl+p" to Telescope Find files
+map('n', '<C-p>', Util.telescope("files"))
+
+-- Map "Ctrl+n" to Telescope Recent files
+map('n', '<c-n>', '<cmd>Telescope oldfiles<cr>')
 
 -- Toggle auto-save
-vim.api.nvim_set_keymap("n", "<leader>n", ":ASToggle<CR>", {})
+map("n", "<leader>n", ":ASToggle<CR>", {})
 
 -- nvim-surround 
 vim.g["surround_no_mappings"] = 1
-vim.keymap.set("n", "ds", "<Plug>Dsurround")
-vim.keymap.set("n", "cs", "<Plug>Csurround")
-vim.keymap.set("n", "cS", "<Plug>CSurround")
-vim.keymap.set("n", "ys", "<Plug>Ysurround")
-vim.keymap.set("n", "yS", "<Plug>YSurround")
-vim.keymap.set("n", "yss", "<Plug>Yssurround")
-vim.keymap.set("n", "ySs", "<Plug>YSsurround")
-vim.keymap.set("n", "ySS", "<Plug>YSsurround")
+map("n", "ds", "<Plug>Dsurround")
+map("n", "cs", "<Plug>Csurround")
+map("n", "cS", "<Plug>CSurround")
+map("n", "ys", "<Plug>Ysurround")
+map("n", "yS", "<Plug>YSurround")
+map("n", "yss", "<Plug>Yssurround")
+map("n", "ySs", "<Plug>YSsurround")
+map("n", "ySS", "<Plug>YSsurround")
 
 -- The conflicting ones. Note that `<Plug>(leap-cross-window)`
 -- _does_ work in Visual mode, if jumping to the same buffer,
 -- so in theory, `gs` could be useful for Leap too...
-vim.keymap.set("x", "gs", "<Plug>VSurround")
-vim.keymap.set("x", "gS", "<Plug>VgSurround")
+map("x", "gs", "<Plug>VSurround")
+map("x", "gS", "<Plug>VgSurround")
