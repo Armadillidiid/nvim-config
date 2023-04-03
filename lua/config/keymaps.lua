b03212dadfe9c89aa0a3a54c9094ab4d047f2bc2
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-03 19:37:14 +0100

    Fixed delete buffer mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,15 +19,18 @@ end
 map("i", "jk", "<Esc>", { noremap = true })
 
 -- Map "Ctrl+p" to Telescope Find files
-map('n', '<C-p>', Util.telescope("files"))
+map("n", "<C-p>", Util.telescope("files"))
 
 -- Map "Ctrl+n" to Telescope Recent files
-map('n', '<c-n>', '<cmd>Telescope oldfiles<cr>')
+map("n", "<c-n>", "<cmd>Telescope oldfiles<cr>")
 
 -- Toggle auto-save
-map("n", "<leader>n", ":ASToggle<CR>", {})
+map("n", "<leader>n", ":ASToggle<CR>", { desc = "Toggle auto-save" })
 
--- nvim-surround 
+--  Map <leader><space> to delete buffer
+map("n", "<leader><space>", ':lua require("mini.bufremove").delete(0, false)<CR>', { desc = "Delete buffer" })
+
+-- nvim-surround
 vim.g["surround_no_mappings"] = 1
 map("n", "ds", "<Plug>Dsurround")
 map("n", "cs", "<Plug>Csurround")
