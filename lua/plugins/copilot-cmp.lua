ee4399af6ba018cd7eb0c233587b1236ff125a6a
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-08 09:10:42 +0100

    Added highlight to copilot suggestions and enabled cmp

--- lua/plugins/copilot-cmp.lua
+++ lua/plugins/copilot-cmp.lua
@@ -1,6 +1,6 @@
 return {
   "zbirenbaum/copilot-cmp",
-  enabled  = false,
+  enabled  = true,
   dependencies = "copilot.lua",
   opts = {},
   config = function(_, opts)
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,21 +1,20 @@
+vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#83a598" })
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      panel = { enabled = true },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<M-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    panel = { enabled = true },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = "<M-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
       },
     },
   },
