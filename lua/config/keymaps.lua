3f14e669636113174f8c3c706439fb13c9d097ce
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-08 09:09:40 +0100

    Fixed nvim-surround not working bug

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -44,18 +44,18 @@ map("v", "<leader>cp", ":Chat please fix any bugs and errors in the following co
 map("x", "<leader>cp", ":Chat please fix any bugs and errors in the following code<CR>", { desc = "ChatGPTProblem" })
 
 -- nvim-surround
-vim.g["surround_no_mappings"] = 1
-map("n", "ds", "<Plug>Dsurround")
-map("n", "cs", "<Plug>Csurround")
-map("n", "cS", "<Plug>CSurround")
-map("n", "ys", "<Plug>Ysurround")
-map("n", "yS", "<Plug>YSurround")
-map("n", "yss", "<Plug>Yssurround")
-map("n", "ySs", "<Plug>YSsurround")
-map("n", "ySS", "<Plug>YSsurround")
-
--- The conflicting ones. Note that `<Plug>(leap-cross-window)`
--- _does_ work in Visual mode, if jumping to the same buffer,
--- so in theory, `gs` could be useful for Leap too...
-map("x", "gs", "<Plug>VSurround")
-map("x", "gS", "<Plug>VgSurround")
+-- vim.g["surround_no_mappings"] = 1
+-- vim.keymap.set("n", "ds", "<Plug>Dsurround")
+-- map("n", "cs", "<Plug>Csurround")
+-- map("n", "cS", "<Plug>CSurround")
+-- map("n", "ys", "<Plug>Ysurround")
+-- map("n", "yS", "<Plug>YSurround")
+-- map("n", "yss", "<Plug>Yssurround")
+-- map("n", "ySs", "<Plug>YSsurround")
+-- map("n", "ySS", "<Plug>YSsurround")
+--
+-- -- The conflicting ones. Note that `<Plug>(leap-cross-window)`
+-- -- _does_ work in Visual mode, if jumping to the same buffer,
+-- -- so in theory, `gs` could be useful for Leap too...
+-- map("x", "gs", "<Plug>VSurround")
+-- map("x", "gS", "<Plug>VgSurround")
